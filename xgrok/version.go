c01577f325bc8db1125d2149ab8eda1a482c0bfe
Kohki Makimoto <kohki.makimoto@gmail.com>
2016-11-22 19:04:22 +0900

    update

--- xgrok/version.go
+++ xgrok/version.go
@@ -10,7 +10,7 @@ var (
 var (
 	DefaultTunnelAddr  = "0.0.0.0:9690"
 	DefaultHttpAddr    = "0.0.0.0:9680"
-	DefaultServerAddr  = "127.0.0.1" + DefaultTunnelAddr
+	DefaultServerAddr  = "127.0.0.1:9690"
 	DefaultInspectAddr = "disabled"
 	// DefaultInspectAddr = "127.0.0.1:4040"
 )
